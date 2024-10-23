1a44cf809d695486fcc7dab53f8dc19b9b34298c
P1wko <miminios20611@gmail.com>
2024-10-23 18:43:01 +0200

    Navigation thru certain games

---
+++ Casino_game/Assets/Scripts/GamesEnum.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public enum Games{
+	Texas,
+	BlackJack,
+	Szwindel
+}
--- Casino_game/Assets/Scripts/MainMenuController.cs
+++ Casino_game/Assets/Scripts/MainMenuController.cs
@@ -1,27 +1,81 @@
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+	public Dictionary<string, GameObject> canvasDictionary = new Dictionary<string, GameObject>();
+
+	private GameObject currentCanvas;
+	private GameObject lastCanvas;
+	private Games chosenGame = Games.Texas;
+
+	public GameObject[] canvasList;
 	public CanvasGroup OptionsPanel;
 	public CanvasGroup ChoosePokerPanel;
 
+	private void Start()
+	{
+		foreach (GameObject canvas in canvasList)
+		{
+			canvasDictionary.Add(canvas.name, canvas);
+			canvas.SetActive(false);
+		}
+
+		ShowCanvas("MainMenuCanvas");
+	}
+
+	public void ShowCanvas(string canvasName)
+	{
+		if (canvasDictionary.ContainsKey(canvasName))
+		{
+			if (currentCanvas != null)
+			{
+				lastCanvas = currentCanvas;
+				currentCanvas.SetActive(false);
+			}
+
+			currentCanvas = canvasDictionary[canvasName];
+			currentCanvas.SetActive(true);
+		}
+		else
+		{
+			Debug.LogWarning("Canvas at name: " + canvasName + " not found");
+		}
+	}
+
+	public void SwitchGame(bool prev = false)
+	{
+		if (!prev) chosenGame = (Games)(((int)chosenGame + 1) % 3);
+		else chosenGame = (Games)(((int)chosenGame - 1) % 3);
+
+		switch (chosenGame)
+		{
+			case Games.Texas:
+				ShowCanvas("ChooseTexasCanvas");
+				break;
+			case Games.BlackJack:
+				ShowCanvas("ChooseBlackjackCanvas");
+				break;
+			case Games.Szwindel:
+				ShowCanvas("ChooseSzwindelCanvas");
+				break;
+			default: break;
+		}
+	}
+
 	public void PlayGame()
 	{
-		ChoosePokerPanel.alpha = 1;
-		ChoosePokerPanel.blocksRaycasts = true;
+		ShowCanvas("ChooseTexasCanvas");
 	}
 
 	public void Option()
 	{
-		OptionsPanel.alpha = 1;
-		OptionsPanel.blocksRaycasts = true;
+		ShowCanvas("OptionsCanvas");
 	}
 
 	public void Back()
 	{
-		OptionsPanel.alpha = 0;
-		OptionsPanel.blocksRaycasts = false;
+		ShowCanvas(lastCanvas.name);
 	}
 
 	public void Exit()
